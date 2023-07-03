b8f619f6b36d1bc5271b6a6d892ac31576dd1ee2
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-04 02:41:51 +0300

    fix: clear fake data

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,6 @@
 import { User } from '../types/User.js';
 
-export let users: User[] = [
-  { id: '109156be-c4fb-41ea-b1b4-efe1671c5836', username: 'Sol', age: 38, hobbies: ['sleep', 'eat'] },
-];
+export let users: User[] = [];
 
 export const removeUserFromDbById = (id: string): void => {
   users = users.filter((element) => element.id !== id);
